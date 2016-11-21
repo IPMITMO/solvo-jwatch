ae79479106f2df8af15d388044b4f77a2b950a38
Artyom Krikheli <deest1ke2012@gmail.com>
2016-11-21 22:41:41 +0300

    fixed comments (template)

--- exe/src/main/java/solvo/jwatch/SystemManagerAPIInstance.java
+++ exe/src/main/java/solvo/jwatch/SystemManagerAPIInstance.java
@@ -1,8 +1,12 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright 2015 by Solvo, LTD
+ *
+ * This program is commercial software.
+ * Any unauthorized use is prohibited.
+ *
+ * Created Sep 29, 2015
  */
+
 package solvo.jwatch;
 
 /**
